c551788de2ced46ed56b6642cb8fe19e6b92c08d
temoncher <temoncher@yandex.ru>
2020-11-02 15:30:25 +0300

    fix update balance trigger

--- Scripts/SetupTriggers.sql
+++ Scripts/SetupTriggers.sql
@@ -1,53 +1,55 @@
 USE [telephone_company]
 GO
-;
 
 -- Create trigger to add account for each new subscriber
 CREATE TRIGGER [TR_subscribers_AfterInsert] ON [subscribers]
 AFTER
 INSERT
   AS BEGIN
-INSERT INTO
-  [accounts] ([subscriber_id])
-SELECT
-  [subscriber_id]
-FROM
-  inserted
+  INSERT INTO
+  [accounts]
+    ([subscriber_id])
+  SELECT
+    [subscriber_id]
+  FROM
+    [inserted]
 END;
 
 GO
-;
 
 -- Create trigger to add account for each new subscriber
 CREATE TRIGGER [TR_transactions_AfterInsert] ON [transactions]
 AFTER
 INSERT
-  AS BEGIN DECLARE @accountId INT,
-  @amount INT,
-  @transactionType VARCHAR,
-  @previousBalance INT
-SELECT
-  @accountId = account_id,
-  @amount = amount,
-  @transactionType = transaction_type_id
-FROM
-  inserted -- TODO: join inserted with transaction types
-SELECT
-  @previousBalance = balance
-FROM
-  [accounts]
-WHERE
-  [account_id] = @accountId
-UPDATE
-  [accounts]
-SET
-  [balance] = CASE
-    WHEN @transactionType = 'INCOME' THEN @previousBalance + @amount
-    ELSE @previousBalance - @amount
-  END
-WHERE
-  [account_id] = @accountId
+  AS BEGIN
+  DECLARE @newInserted TABLE(
+    account_id INT NOT NULL,
+    income INT NOT NULL,
+    loss INT NOT NULL
+  )
+
+  INSERT INTO @newInserted
+  SELECT
+    [inserted].[account_id],
+    SUM(CASE WHEN [transaction_types].[title] = 'INCOME' THEN [inserted].[amount] ELSE 0 END) [income],
+    SUM(CASE WHEN [transaction_types].[title] = 'LOSS' THEN [inserted].[amount] ELSE 0 END) [loss]
+  FROM
+    [inserted]
+    JOIN [transaction_types] ON [inserted].[transaction_type_id] = [transaction_types].[transaction_type_id]
+  GROUP BY [inserted].[account_id]
+
+  UPDATE
+    [accounts]
+  SET
+    [balance] = (
+      SELECT [@newInserted].[income] - [@newInserted].[loss]
+  FROM @newInserted
+  WHERE [accounts].[account_id] = [@newInserted].[account_id]
+    )
+  WHERE [accounts].[account_id] IN (
+    SELECT DISTINCT [@newInserted].[account_id]
+  FROM @newInserted
+  )
 END;
 
 GO
-;

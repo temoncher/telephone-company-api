d37eaa89269354d9009ed4abb4bca06769291fc0
temoncher <temoncher@yandex.ru>
2020-11-02 11:14:16 +0300

    update triggers

--- Scripts/CreateTables.sql
+++ Scripts/CreateTables.sql
@@ -33,12 +33,13 @@ GO
 -- Create Subscribers table
 CREATE TABLE [subscribers] (
   [subscriber_id] INT IDENTITY(1, 1) PRIMARY KEY,
-  [organisation_id] INT NOT NULL FOREIGN KEY REFERENCES [organisations](organisation_id),
+  [organisation_id] INT NOT NULL,
   [inn] INT NOT NULL,
   [first_name] NVARCHAR(30) NOT NULL,
   [last_name] NVARCHAR(30) NOT NULL,
   [patronymic] NVARCHAR(30),
-  [adress] NVARCHAR(50)
+  [adress] NVARCHAR(50),
+  FOREIGN KEY(organisation_id) REFERENCES [organisations](organisation_id)
 );
 
 GO
@@ -47,8 +48,9 @@ GO
 -- Create Accounts table
 CREATE TABLE [accounts] (
   [account_id] INT IDENTITY(1, 1) PRIMARY KEY,
-  [subscriber_id] INT NOT NULL FOREIGN KEY REFERENCES [subscribers](subscriber_id),
+  [subscriber_id] INT NOT NULL,
   [balance] INT DEFAULT 0,
+  FOREIGN KEY(subscriber_id) REFERENCES [subscribers](subscriber_id)
 );
 
 GO
@@ -66,10 +68,12 @@ GO
 -- Create Transactions table
 CREATE TABLE [transactions] (
   [transaction_id] INT IDENTITY(1, 1) PRIMARY KEY,
-  [transaction_type_id] INT NOT NULL FOREIGN KEY REFERENCES [transaction_types](transaction_type_id),
-  [account_id] INT NOT NULL FOREIGN KEY REFERENCES [accounts](account_id),
+  [transaction_type_id] INT NOT NULL,
+  [account_id] INT NOT NULL,
   [amount] INT NOT NULL,
   [timestamp] DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY(transaction_type_id) REFERENCES [transaction_types](transaction_type_id),
+  FOREIGN KEY(account_id) REFERENCES [accounts](account_id)
 );
 
 GO
--- Scripts/SetupTriggers.sql
+++ Scripts/SetupTriggers.sql
@@ -3,7 +3,7 @@ GO
 ;
 
 -- Create trigger to add account for each new subscriber
-CREATE TRIGGER [TR_subscribers_InsteadOfInsert] ON [subscribers]
+CREATE TRIGGER [TR_subscribers_AfterInsert] ON [subscribers]
 AFTER
 INSERT
   AS BEGIN
@@ -13,6 +13,40 @@ SELECT
   [subscriber_id]
 FROM
   inserted
+END;
+
+GO
+;
+
+-- Create trigger to add account for each new subscriber
+CREATE TRIGGER [TR_transactions_AfterInsert] ON [transactions]
+AFTER
+INSERT
+  AS BEGIN DECLARE @accountId INT,
+  @amount INT,
+  @transactionType VARCHAR,
+  @previousBalance INT
+SELECT
+  @accountId = account_id,
+  @amount = amount,
+  @transactionType = transaction_type_id
+FROM
+  inserted -- TODO: join inserted with transaction types
+SELECT
+  @previousBalance = balance
+FROM
+  [accounts]
+WHERE
+  [account_id] = @accountId
+UPDATE
+  [accounts]
+SET
+  [balance] = CASE
+    WHEN @transactionType = 'INCOME' THEN @previousBalance + @amount
+    ELSE @previousBalance - @amount
+  END
+WHERE
+  [account_id] = @accountId
 END;
 
 GO

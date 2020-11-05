a82840f8eb6354075771a9004a5fc0ac963b9e17
temoncher <temoncher@yandex.ru>
2020-11-05 06:53:21 +0300

    prettify accounts table view

--- Scripts/Views/AccountsGlobalView.sql
+++ Scripts/Views/AccountsGlobalView.sql
@@ -4,7 +4,11 @@ SELECT
   [account_id],
   [balance],
   [subscribers].[subscriber_id],
-  [subscriber_full_name] = CONCAT([subscribers].[first_name], ' ', [subscribers].[last_name])
+  [subscriber_full_name] = CONCAT(
+    [subscribers].[first_name],
+    ' ',
+    [subscribers].[last_name]
+  )
 FROM [accounts]
   JOIN [subscribers]
   ON [subscribers].[subscriber_id] = [accounts].[subscriber_id]

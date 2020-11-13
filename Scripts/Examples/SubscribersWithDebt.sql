6b10db194ff63eae65152e80ccb0d41ef32df6e8
temoncher <temoncher@yandex.ru>
2020-11-13 10:49:19 +0300

    fix comment

--- Scripts/Examples/SubscribersWithDebt.sql
+++ Scripts/Examples/SubscribersWithDebt.sql
@@ -18,7 +18,7 @@ SELECT TOP 3
 	[subscribers].[patronymic],
 	[subscribers].[adress],
 	ABS([accounts].[balance]) AS debt,
-	 -- I didn't really get how do I calculate fine, so I just multipled balance by random number
+	 -- I didn't really get how to calculate fine, so I just multipled balance by random number
 	ABS([accounts].[balance]) * 0.08 AS fine
 FROM [subscribers]
 JOIN [accounts] ON [subscribers].[subscriber_id] = [accounts].[subscriber_id]
